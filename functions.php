5f0cfe766d9a9bacacba0372aabc917a9865630b
mrmatti3 <matt@iputty.com>
2013-10-21 10:22:31 -0700

    Adding custom js file to parent
    in prep of modification of bootstrap dropdown changes

--- functions.php
+++ functions.php
@@ -27,3 +27,11 @@ if(function_exists('register_nav_menus')){
  			)
  		);	
 }
+
+//load up custom js files.
+
+function frame_load_custom_js(){
+	wp_register_script('custom_js', get_template_directory_uri().'/core/js/frameCus.js', array("jquery"), '1.0.0', true);
+}	wp_enqueue_script('custom_js');
+
+add_action('init','frame_load_custom_js');
