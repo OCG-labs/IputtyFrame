f4d428d65d86d77fe902b4d232ec01ef7f5b1db1
Matthew Hansen <mattie02@gmail.com>
2013-11-24 09:26:01 -0800

    Update searchform.php
    removed un-needed structure

--- searchform.php
+++ searchform.php
@@ -1,11 +1,9 @@
-<div class="col-md-6 col-md-offset-3">
-   <form action="/" method="get"> 
-   		<div class="input-group">
-		  <input type="text" class="form-control" name="s" id="search" value="<?php the_search_query(); ?>" placeholder="Try Our Search">
-		  <span class="input-group-btn">
-		    <button class="btn btn-info" type="submit">Search</button>
-		  </span>
-		</div><!-- /input-group -->
-	</form>
-  </div><!-- /.col-md-6 -->
-</div><!-- /.row -->
+<form action="/" method="get"> 
+	<div class="input-group">
+		<input type="text" class="form-control" name="s" id="search" value="<?php the_search_query(); ?>" placeholder="Try Our Search">
+		<span class="input-group-btn">
+			<button class="btn btn-info" type="submit">Search</button>
+		</span>
+	</div><!-- /input-group -->
+</form>
+  
