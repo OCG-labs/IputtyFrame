1f23a4b170e1e95be60364357bd9edd0e20a85c1
Matthew Hansen <mattie02@gmail.com>
2013-11-25 10:05:28 -0800

    Update searchform.php
    Changed placeholder text and search button to icon

--- searchform.php
+++ searchform.php
@@ -1,8 +1,8 @@
 <form action="<?php bloginfo('url'); ?>" method="get"> 
 	<div class="input-group">
-		<input type="text" class="form-control" name="s" id="search" value="<?php the_search_query(); ?>" placeholder="Try Our Search">
+		<input type="text" class="form-control" name="s" id="search" value="<?php the_search_query(); ?>" placeholder="Search">
 		<span class="input-group-btn">
-			<button class="btn btn-info" type="submit">Search</button>
+			<button class="btn btn-info" type="submit"><i class="fa fa-search"></i></button>
 		</span>
 	</div><!-- /input-group -->
 </form>
