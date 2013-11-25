88143f2eeb1ba4e950ffb84627e939f6fff256c0
Matthew Hansen <mattie02@gmail.com>
2013-11-25 11:39:21 -0800

    Update searchform.php
    compatibility updates

--- searchform.php
+++ searchform.php
@@ -2,7 +2,7 @@
 	<div class="input-group">
 		<input type="text" class="form-control" name="s" id="search" value="<?php the_search_query(); ?>" placeholder="Search">
 		<span class="input-group-btn">
-			<button class="btn btn-info" type="submit"><i class="fa fa-search"></i></button>
+			<button class="btn btn-info" type="submit"><span class="fa fa-search"></span></button>
 		</span>
 	</div><!-- /input-group -->
 </form>
