70ce5764c4fc71310261b7883fc41e15950b07e1
Matthew Hansen <mattie02@gmail.com>
2013-12-23 12:05:08 -0800

    Update frameCus.js
    add fancybox script

--- core/js/frameCus.js
+++ core/js/frameCus.js
@@ -30,3 +30,19 @@ jQuery(function() {
     });
 });
 
+
+jQuery(document).ready(function() {
+ jQuery(".various").fancybox({
+   maxWidth	: 800,
+   maxHeight	: 600,
+   fitToView	: false,
+   width		: '70%',
+   height		: '70%',
+   autoSize	: false,
+   closeClick	: false,
+   openEffect	: 'none',
+   closeEffect	: 'none'
+ });
+});
+
+
